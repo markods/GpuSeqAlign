ad398aa14169d5dea4badfaf92ac3f89b45ed29b
Marko Stanojević <marko.d.stanojevic@gmail.com>
2025-04-05 16:44:08 +0200

    print_cmd_usage gets an output stream parameter

--- src/main.cpp
+++ src/main.cpp
@@ -12,45 +12,45 @@
 #include <sstream>
 #include <string>
 
-void print_cmd_usage()
+void print_cmd_usage(std::ostream& os)
 {
-    std::cout << "nw --algParamPath \"path\" --seqPath \"path\" [params]\n"
-                 "\n"
-                 "Parameters:\n"
-                 "-b, --substPath            Path of JSON substitution matrices file, defaults to \"./resrc/subst.json\".\n"
-                 "-r, --algParamPath         Path of JSON algorithm parameters file.\n"
-                 "-s, --seqPath              Path of FASTA file with sequences to be aligned.\n"
-                 "-p, --pairPath             Path of TXT file with sequence pairs to be aligned. Each line is in the format\n"
-                 "                           \"seqA[0:42] seqB\", where \"seqA\" and \"seqB\" are sequence ids, and \"[a:b]\" specifies the\n"
-                 "                           substring starting from element \"a\" (inclusive) until element \"b\" (exclusive).\n"
-                 "                           It's possible to omit the start/end of the interval, like so: \"[a:b]\", \"[a:]\", \"[:b]\".\n"
-                 "                           If the TXT file is not specified, then all sequences in the FASTA file except the first\n"
-                 "                           are aligned to the first sequence. If there is only one sequence in the FASTA file,\n"
-                 "                           it's aligned with itself.\n"
-                 "-o, --resPath              Path of JSON test bench results file, defaults to \"./logs/<datetime>.json\".\n"
-                 "\n"
-                 "--substName                Specify which substitution matrix from the \"subst\" file will be used. Defaults to\n"
-                 "                           \"blosum62\".\n"
-                 "--gapoCost                 Gap open cost, defaults to 11.\n"
-                 "--gapeCost                 Unused, defaults to 0.\n"
-                 "--algName                  Specify which algorithm from the \"algParam\" JSON file will be used. Can be specified\n"
-                 "                           multiple times, in which case those algorithms will be used, in that order.\n"
-                 "                           If not specified, all algorithms in the \"algParam\" JSON file are used, in that order.\n"
-                 "--refAlgName               Specify the algorithm name which should be considered as the source of truth.\n"
-                 "                           If not specified, defaults to the first algorithm in the \"algParam\" JSON file.\n"
-                 "--warmupPerAlign           Number of warmup runs per alignments. Defaults to 0.\n"
-                 "--samplesPerAlign          Number of runs per alignment. Defaults to 1.\n"
-                 "\n"
-                 "--calcTrace                Should the trace be calculated. Defaults to true.\n"
-                 "--calcScoreHash            Should the score matrix hash be calculated. Used to verify correctness with the reference\n"
-                 "                           algorithm implementation. Defaults to false.\n"
-                 "--debugPath                For debug purposes, path of the TXT file where score matrices/traces will be\n"
-                 "                           written to, once per alignment. Defaults to \"\".\n"
-                 "--printScore               Should the score matrix be printed. Defaults to false.\n"
-                 "--printTrace               Should the trace be printed. Defaults to false.\n"
-                 "\n"
-                 "-h, --help                 Print help and exit.\n"
-                 "\n";
+    os << "nw --algParamPath \"path\" --seqPath \"path\" [params]\n"
+          "\n"
+          "Parameters:\n"
+          "-b, --substPath            Path of JSON substitution matrices file, defaults to \"./resrc/subst.json\".\n"
+          "-r, --algParamPath         Path of JSON algorithm parameters file.\n"
+          "-s, --seqPath              Path of FASTA file with sequences to be aligned.\n"
+          "-p, --pairPath             Path of TXT file with sequence pairs to be aligned. Each line is in the format\n"
+          "                           \"seqA[0:42] seqB\", where \"seqA\" and \"seqB\" are sequence ids, and \"[a:b]\" specifies the\n"
+          "                           substring starting from element \"a\" (inclusive) until element \"b\" (exclusive).\n"
+          "                           It's possible to omit the start/end of the interval, like so: \"[a:b]\", \"[a:]\", \"[:b]\".\n"
+          "                           If the TXT file is not specified, then all sequences in the FASTA file except the first\n"
+          "                           are aligned to the first sequence. If there is only one sequence in the FASTA file,\n"
+          "                           it's aligned with itself.\n"
+          "-o, --resPath              Path of JSON test bench results file, defaults to \"./logs/<datetime>.json\".\n"
+          "\n"
+          "--substName                Specify which substitution matrix from the \"subst\" file will be used. Defaults to\n"
+          "                           \"blosum62\".\n"
+          "--gapoCost                 Gap open cost, defaults to 11.\n"
+          "--gapeCost                 Unused, defaults to 0.\n"
+          "--algName                  Specify which algorithm from the \"algParam\" JSON file will be used. Can be specified\n"
+          "                           multiple times, in which case those algorithms will be used, in that order.\n"
+          "                           If not specified, all algorithms in the \"algParam\" JSON file are used, in that order.\n"
+          "--refAlgName               Specify the algorithm name which should be considered as the source of truth.\n"
+          "                           If not specified, defaults to the first algorithm in the \"algParam\" JSON file.\n"
+          "--warmupPerAlign           Number of warmup runs per alignments. Defaults to 0.\n"
+          "--samplesPerAlign          Number of runs per alignment. Defaults to 1.\n"
+          "\n"
+          "--calcTrace                Should the trace be calculated. Defaults to true.\n"
+          "--calcScoreHash            Should the score matrix hash be calculated. Used to verify correctness with the reference\n"
+          "                           algorithm implementation. Defaults to false.\n"
+          "--debugPath                For debug purposes, path of the TXT file where score matrices/traces will be\n"
+          "                           written to, once per alignment. Defaults to \"\".\n"
+          "--printScore               Should the score matrix be printed. Defaults to false.\n"
+          "--printTrace               Should the trace be printed. Defaults to false.\n"
+          "\n"
+          "-h, --help                 Print help and exit.\n"
+          "\n";
 }
 
 // get the current time as an ISO string
@@ -435,7 +435,7 @@ int main(int argc, char* argv[])
         }
         else if (arg == "-h" || arg == "--help")
         {
-            print_cmd_usage();
+            print_cmd_usage(std::cout);
             return 0;
         }
         else
