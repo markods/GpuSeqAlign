d4ab39590cb04781d2ab2a7103e1c2cabb80c8bb
Marko Stanojević <marko.d.stanojevic@gmail.com>
2025-04-05 18:30:33 +0200

    move defer to lang.hpp

--- src/common.hpp
+++ src/common.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_COMMON_HPP
 
 #include "fmt_guard.hpp"
+#include "lang.hpp"
 #include "memory.hpp"
 #include <chrono>
 #include <cuda_runtime.h>
@@ -9,68 +10,6 @@
 #include <string>
 #include <utility>
 #include <vector>
-
-// defer execution to scope exit
-template <typename F>
-class Defer
-{
-    // static_assert(std::is_nothrow_invocable_r_v<void, F>, "F must be a callable type with signature void() noexcept");
-
-public:
-    explicit Defer(F _func) noexcept
-        : func {std::move(_func)}, active {true}
-    { }
-
-    Defer(const Defer&) = delete;
-    Defer& operator=(const Defer&) = delete;
-
-    Defer(Defer&& other) noexcept
-        : func {std::move(other.func)}, active {other.active}
-    {
-        // The moved-from object must not run the function.
-        other.active = false;
-    }
-    Defer& operator=(Defer&& other) noexcept
-    {
-        if (this != &other)
-        {
-            func = std::move(other.func);
-            active = other.active;
-            other.active = false;
-        }
-        return *this;
-    }
-
-    void operator()() noexcept
-    {
-        doDefer();
-    }
-
-    ~Defer() noexcept
-    {
-        doDefer();
-    }
-
-private:
-    void doDefer() noexcept
-    {
-        if (active)
-        {
-            active = false;
-            func();
-        }
-    }
-
-private:
-    F func;
-    bool active;
-};
-
-template <typename F>
-Defer<F> make_defer(F _func)
-{
-    return Defer<F>(std::move(_func));
-}
 
 // get the specified element from the given linearized matrix
 #define el(mat, cols, i, j) (mat[(cols) * (i) + (j)])
---
+++ src/lang.hpp
@@ -0,0 +1,66 @@
+#ifndef INCLUDE_LANG_HPP
+#define INCLUDE_LANG_HPP
+
+// defer execution to scope exit
+template <typename F>
+class Defer
+{
+    // static_assert(std::is_nothrow_invocable_r_v<void, F>, "F must be a callable type with signature void() noexcept");
+
+public:
+    explicit Defer(F _func) noexcept
+        : func {std::move(_func)}, active {true}
+    { }
+
+    Defer(const Defer&) = delete;
+    Defer& operator=(const Defer&) = delete;
+
+    Defer(Defer&& other) noexcept
+        : func {std::move(other.func)}, active {other.active}
+    {
+        // The moved-from object must not run the function.
+        other.active = false;
+    }
+    Defer& operator=(Defer&& other) noexcept
+    {
+        if (this != &other)
+        {
+            func = std::move(other.func);
+            active = other.active;
+            other.active = false;
+        }
+        return *this;
+    }
+
+    void operator()() noexcept
+    {
+        doDefer();
+    }
+
+    ~Defer() noexcept
+    {
+        doDefer();
+    }
+
+private:
+    void doDefer() noexcept
+    {
+        if (active)
+        {
+            active = false;
+            func();
+        }
+    }
+
+private:
+    F func;
+    bool active;
+};
+
+template <typename F>
+Defer<F> make_defer(F _func)
+{
+    return Defer<F>(std::move(_func));
+}
+
+#endif // INCLUDE_LANG_HPP
