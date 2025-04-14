c3b58158c753dd7fbfa0d94d76ea16fdd1260be7
Marko Stanojević <marko.d.stanojevic@gmail.com>
2025-04-14 21:50:26 +0200

    todos

--- src/run_types.hpp
+++ src/run_types.hpp
@@ -66,8 +66,6 @@ struct NwAlgParams
     bool _isEnd;
 };
 
-// TODO: fix: Exception thrown at 0x00007FF83227016C in nw.exe: Microsoft C++ exception: std::out_of_range at memory location 0x000000A7E93FB4C8.
-
 struct NwAlgInput
 {
     std::vector<int> subst;
